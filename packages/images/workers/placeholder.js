f5060ca4fb295337cf1470ab50823b8beb686aa6
Michael Pope <michael.pope@additel.com>
2021-12-09 15:55:30 -0800

    Added Base64 Image ID to help with Lighthouse.
    Lighthouse sometimes dislikes ElderJS's base64 placeholder pictures.
    Usually, it lets you know that the placeholder needs to be resized.
    Unfortunately, Lighthouse doesn't help you identify the image well.
    Which means on pages with lots of pictures, this is a pain.
    Having an ID embedded in the data URL fixes this.
    It does make pages slightly larger, but I feel the tradeoff is worth it.

--- packages/images/workers/placeholder.js
+++ packages/images/workers/placeholder.js
@@ -1,4 +1,5 @@
 const sharp = require('sharp');
+const { nanoid } = require('nanoid')
 
 module.exports = async ({ rel, src: uncheckedSrc, options, debug }) => {
   try {
@@ -10,9 +11,13 @@ module.exports = async ({ rel, src: uncheckedSrc, options, debug }) => {
 
     const place = await sharp(src).resize(options.resize).jpeg(options.jpeg).toBuffer({ resolveWithObject: false });
 
-    if (debug) console.log({ rel, placeholder: `data:image/jpeg;base64,${place.toString('base64')}`, error: null });
+    //Lighthouse makes it difficutl to tell which placeholders belong to which images, so this helps us know which one it belongs to
+    //We Stick it in the base64 image encoded URL in a place where it does not interfere with the image.
+    const id_string = `id:${nanoid()};`;
 
-    return { rel, placeholder: `data:image/jpeg;base64,${place.toString('base64')}`, error: null };
+    if (debug) console.log({ rel, placeholder: `data:image/jpeg;${id_string}base64,${place.toString('base64')}`, error: null });
+
+    return { rel, placeholder: `data:image/jpeg;${id_string}base64,${place.toString('base64')}`, error: null };
   } catch (e) {
     return { error: e };
   }

798474a1c0f2c69b3894b85a29eac306cc244966
Michael Pope <michael.pope@additel.com>
2021-12-09 15:59:33 -0800

    Formatted the git pull request more nicely.

--- packages/images/workers/placeholder.js
+++ packages/images/workers/placeholder.js
@@ -9,11 +9,11 @@ module.exports = async ({ rel, src: uncheckedSrc, options, debug }) => {
       src = Buffer.from(uncheckedSrc);
     }
 
-    const place = await sharp(src).resize(options.resize).jpeg(options.jpeg).toBuffer({ resolveWithObject: false });
-
     //Lighthouse makes it difficutl to tell which placeholders belong to which images, so this helps us know which one it belongs to
     //We Stick it in the base64 image encoded URL in a place where it does not interfere with the image.
     const id_string = `id:${nanoid()};`;
+
+    const place = await sharp(src).resize(options.resize).jpeg(options.jpeg).toBuffer({ resolveWithObject: false });
 
     if (debug) console.log({ rel, placeholder: `data:image/jpeg;${id_string}base64,${place.toString('base64')}`, error: null });
 

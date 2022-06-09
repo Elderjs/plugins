f237ba181d5270f5b587be1a9d1debf7ed886023
Nick Reese <code@nicholasreese.com>
2022-06-09 15:43:14 +0200

    get random plugin working

--- packages/random/src/index.ts
+++ packages/random/src/index.ts
@@ -1,37 +1,29 @@
-const notProduction = process.env.NODE_ENV !== 'production';
-const plugin = {
+import { PluginOptions } from '@elderjs/elderjs';
+
+const production = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'PRODUCTION';
+const plugin: PluginOptions = {
   name: '@elderjs/plugin-random',
   description: 'Adds a /random/ page on previewer requests that displays all of the possible routes.',
-  minimumElderjsVersion: '1.4.13',
+  minimumElderjsVersion: '1.7.5',
+
   init: (plugin) => {
-    // version check
-    let enabled = false;
-    if (plugin.settings.version) {
-      const pv = plugin.settings.version.split('.');
-      const major = Number(pv[0]);
-      const minor = Number(pv[1]);
-      const patch = Number(pv[2]);
-      if (major > 1) enabled = true;
-      if (major === 1 && minor > 4) enabled = true;
-      if (major === 1 && minor === 4 && patch >= 13) enabled = true;
-    }
-    if (!enabled) {
+    if (production) {
       plugin.hooks = [];
       plugin.routes = {};
-      console.error('@elderjs/plugin-random requires Elder.js v1.4.13 or greater. Plugin disabled.');
     }
     return plugin;
   },
+
   routes: {
     pluginRandom: {
-      data: () => {},
+      data: {},
       template: 'Random.svelte',
       layout: 'RandomLayout.svelte',
       permalink: ({ request }) => {
         if (request.slug && request.realRoute) return `/random/${request.realRoute}/`;
         return `/random/`;
       },
-      all: async ({ settings }) => (settings.server && notProduction ? [{ slug: '/random/' }] : []),
+      all: async ({ settings }) => (settings.server && !production ? [{ slug: '/random/' }] : []),
     },
   },
 
@@ -42,7 +34,7 @@ const plugin = {
       description: 'Adds /random/route/ requests to allRequests. Also adds /debug/ to all requests.',
       priority: 50, // default
       run: async ({ settings, allRequests, routes }) => {
-        if (notProduction && settings.server) {
+        if (!production && settings.server) {
           const randomRequests = Object.keys(routes).reduce(
             (out, route) => [...out, { slug: 'random', realRoute: route, route: 'pluginRandom' }],
             [],
@@ -58,16 +50,22 @@ const plugin = {
       name: 'updateRandomRequestWithRealRequest',
       description: 'Adjusts random requests to go to a real page and route',
       priority: 70, // Higher priority incase other plugins also need to be run on the same request.
-      run: ({ settings, request, allRequests, routes }) => {
-        if (notProduction && settings.server && request.slug === 'random' && request.route === 'pluginRandom') {
+      run: ({ settings, request: priorRequest, allRequests, routes }) => {
+        const { req } = priorRequest;
+        if (!production && settings.server && priorRequest.slug === 'random' && priorRequest.route === 'pluginRandom') {
           // filter requests
-          const routeRequests = allRequests.filter((r) => r.route === request.realRoute);
+          const routeRequests = allRequests.filter((r) => r.route === priorRequest.realRoute);
 
           if (routeRequests && routeRequests.length > 0) {
             // choose a random one
             const randomRequest = Math.floor(Math.random() * (routeRequests.length - 1));
+
             // update the request
-            const request = routeRequests.slice(randomRequest, randomRequest + 1)[0];
+            const selectedRequest = routeRequests.slice(randomRequest, randomRequest + 1)[0];
+
+            // clone to prevent the req object pollution due to pass by reference.
+            const request = JSON.parse(JSON.stringify(selectedRequest));
+            request.req = req;
 
             const route = routes[request.route];
             return {
@@ -83,7 +81,7 @@ const plugin = {
       name: 'addAllRequestsToDebugDataObject',
       description: 'Adds the allRequests array to the data object for /debug/',
       run: ({ data, settings, request, allRequests }) => {
-        if (notProduction && settings.server && request.slug === 'debug' && request.route === 'debug') {
+        if (!production && settings.server && request.slug === 'debug' && request.route === 'debug') {
           return {
             data: Object.assign({}, data, {
               allRequests: [...allRequests],
---
+++ packages/random/svelte/index.js
@@ -0,0 +1,3 @@
+export { default as Random } from './Random.svelte';
+
+export { default as RandomLayout } from './RandomLayout.svelte';
